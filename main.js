979b2178f8af1837710adcad46bd88f6857ea93c
Alhassan Osman <oalhassan847@gmail.com>
2023-01-04 19:11:14 +0000

    fix linter errors

--- main.js
+++ main.js
@@ -3,28 +3,28 @@ const closeBtn = document.querySelector('#close');
 const mobileDrop = document.querySelector('.menu-drop');
 
 function showMenu() {
-    mobileDrop.style.display = 'block';
-    closeBtn.style.display = 'block';
-    menuBtn.style.display = 'none';
+  mobileDrop.style.display = 'block';
+  closeBtn.style.display = 'block';
+  menuBtn.style.display = 'none';
 }
 
 function hideMenu() {
-    menuBtn.style.display = 'block';
-    mobileDrop.style.display = 'none';
-    closeBtn.style.display = 'none';
+  menuBtn.style.display = 'block';
+  mobileDrop.style.display = 'none';
+  closeBtn.style.display = 'none';
 }
 
 menuBtn.addEventListener('click', showMenu);
 closeBtn.addEventListener('click', hideMenu);
 
 function myFunction(desktopView) {
-    if (desktopView.matches) {
-        mobileDrop.style.display = 'none';
-        closeBtn.style.display = 'none';
-    } else {
-        menuBtn.style.display = 'block';
-        mobileDrop.style.display = 'none';
-    }
+  if (desktopView.matches) {
+    mobileDrop.style.display = 'none';
+    closeBtn.style.display = 'none';
+  } else {
+    menuBtn.style.display = 'block';
+    mobileDrop.style.display = 'none';
+  }
 }
 
 const desktopView = window.matchMedia('(min-width: 768px)');
