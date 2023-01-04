3cc1f8d9a6c2fa3c5a0a4442c89ceaef0d84ea70
Alhassan Osman <oalhassan847@gmail.com>
2023-01-04 17:31:41 +0000

    add event to hide menu drop in desktop

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ function hideMenu() {
 
 menuBtn.addEventListener('click', showMenu);
 closeBtn.addEventListener('click', hideMenu);
+
+function myFunction(desktopView) {
+    if (desktopView.matches) {
+        mobileDrop.style.display = 'none';
+        closeBtn.style.display = 'none';
+    } else {
+      menuBtn.style.display = 'block';
+      mobileDrop.style.display = 'none';
+    }
+  }
+  
+const desktopView = window.matchMedia('(min-width: 768px)');
+myFunction(desktopView);
+
+desktopView.addListener(myFunction);
